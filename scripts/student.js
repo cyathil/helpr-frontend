17da80823960250915ba7c733d080b85fab2d5b2
Tim Fan <tfan478@gmail.com>
2020-07-06 19:29:57 +1000

    feat: redirect user to login if invalid.

--- scripts/student.js
+++ scripts/student.js
@@ -5,47 +5,32 @@
 backendURL = "http://cyathil.pythonanywhere.com";
 
 ////////////////////////////////////////////////////////////////////////////////
-// bootstrap on page load.
+// bootstrap.
 ////////////////////////////////////////////////////////////////////////////////
 
 // get current login.
 const isStudent = window.sessionStorage.getItem("isStudent");
 const zid = window.sessionStorage.getItem("zid");
 
-if (zid && isStudent) {
-	/* current login is valid student */
+// if current login is invalid.
+if (!(zid && isStudent)) {
+	// redirect user to the login page.
+	window.location.href = "/";
+}
 
-	/* fill in greeting. */
+// otherwise, perform callback function on page load.
+window.onload = function() {
+	// current login is valid student.
+	
+	// fill in greeting.
 	document.getElementById("greeting").appendChild(function () {
 		const p = document.createElement("p");
 		p.appendChild(document.createTextNode(`hello student ${zid}.`));
 		return p;
 	}());
 
-	/* fill in make request form */
-	const makeRequestForm = document.getElementById("makeRequestForm");
-	// <h2>make request form</h2>
-	const h2 = document.createElement("h2");
-	h2.appendChild(document.createTextNode("make request form"));
-	// <p>
-	// 	<label for="description">description:</label>
-	// 	<input type="text" name="description">
-	// </p>
-	const p = document.createElement("p");
-	const label = document.createElement("label");
-	label.setAttribute("for", "description");
-	label.appendChild(document.createTextNode("description: "));
-	const input = document.createElement("input");
-	input.setAttribute("type", "text");
-	input.setAttribute("name", "description");
-	p.appendChild(label);
-	p.appendChild(input);
-	// inserting created elements into form.
-	makeRequestForm.appendChild(h2);
-	makeRequestForm.appendChild(p);
-
 	// event listener for make request form.
-	makeRequestForm.addEventListener("submit", event => {
+	document.getElementById("makeRequestForm").addEventListener("submit", event => {
 		event.preventDefault();
 		// create json request body from makeRequestForm information.
 		const description = makeRequestForm["description"].value;
@@ -59,24 +44,5 @@ if (zid && isStudent) {
 		xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 		xmlhttp.send(requestBody);
 	});
-
-} else {
-	/* current login is invalid student */
-
-	// fill in greeting.
-	document.getElementById("greeting").appendChild(function () {
-		const p = document.createElement("p");
-		console.log(typeof (p));
-		// description of invalid login error.
-		p.appendChild(document.createTextNode("boohoo. please visit "));
-		// link to visit to login.
-		const failLink = document.createElement("a");
-		failLink.setAttribute("href", "/");
-		failLink.appendChild(document.createTextNode("this"));
-		p.appendChild(failLink);
-		// continued description.
-		p.appendChild(document.createTextNode(" link to login."));
-		return p;
-	}())
 
 }
