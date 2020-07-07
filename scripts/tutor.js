6e3bfaf9c44a2722b8ec5fdb66415d7fc3ea781b
Tim Fan <tfan478@gmail.com>
2020-07-07 10:14:56 +1000

    feat: add tutor homepage.

--- scripts/tutor.js
+++ scripts/tutor.js
@@ -1,30 +1,31 @@
-// get current tutor login.
+///////////////////////////////////////////////////////////
+// global variables.
+///////////////////////////////////////////////////////////
+
+backendURL = "http://cyathil.pythonanywhere.com";
+
+////////////////////////////////////////////////////////////////////////////////
+// bootstrap.
+////////////////////////////////////////////////////////////////////////////////
+
+// get current login.
 const isTutor = window.sessionStorage.getItem("isTutor");
 const zid = window.sessionStorage.getItem("zid");
-// set greeting based on login status.
-const element = (zid && isTutor) ? generateSuccessElement() : generateFailElement();
-document.getElementById("greeting").appendChild(element);
 
-/**
- * returns the element to append if valid tutor login.
- */
-function generateSuccessElement() {
-	return document.createTextNode(`hello tutor ${zid}.`);
+// if current login is invalid.
+if (!(zid && isTutor)) {
+	// redirect user to the login page.
+	window.location.href = "/";
 }
 
-/**
- * returns the element to append if invalid tutor login.
- */
-function generateFailElement() {
-	const failElement = document.createElement("p");
-	// description of invalid login error.
-	failElement.appendChild(document.createTextNode("boohoo. please visit "));
-	// link to visit to login.
-	const failLink = document.createElement("a");
-	failLink.setAttribute("href", "/");
-	failLink.appendChild(document.createTextNode("this"));
-	failElement.appendChild(failLink);
-	// continued description.
-	failElement.appendChild(document.createTextNode(" link to login."));
-	return failElement;
+// otherwise, perform callback function on page load.
+window.onload = function () {
+
+	// fill in greeting.
+	document.getElementById("greeting").appendChild(function () {
+		const p = document.createElement("p");
+		p.appendChild(document.createTextNode(`hello tutor ${zid}.`));
+		return p;
+	}());
+
 }
